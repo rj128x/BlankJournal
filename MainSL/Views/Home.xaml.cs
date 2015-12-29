94bb80854568bf2ebc90c5dec5fae8497560f959
rj128x <rj128x@gmail.com>
2015-12-29 10:31:50 +0500

    1111

--- MainSL/Views/Home.xaml.cs
+++ MainSL/Views/Home.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 namespace MainSL {
 	public partial class Home : Page {
 		public TBPInfo CurrentTBP { get; set; }
+
 		public Home() {
 			InitializeComponent();
 			init();
@@ -46,8 +47,10 @@ namespace MainSL {
 			GlobalContext.Single.IsBusy = false;
 			ReturnMessage msg = e.Result;
 			MessageBox.Show(msg.Message);
-			GlobalContext.Single.IsBusy = true;
-			GlobalContext.Single.Client.GetTBPBlanksByFolderAsync(CurrentFolder.ID);
+			if (CurrentFolder != null) {
+				GlobalContext.Single.IsBusy = true;
+				GlobalContext.Single.Client.GetTBPBlanksByFolderAsync(CurrentFolder.ID);
+			}
 		}
 
 
@@ -88,15 +91,17 @@ namespace MainSL {
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e) {
-			TBPInfo newBlank = new TBPInfo();
-			newBlank.Number = "-";
-			newBlank.Name = "";
-			newBlank.FolderID = CurrentFolder.ID;
-			TBPWindow newWindow = new TBPWindow();
-			newBlank.EditingTBP = false;
-			newWindow.Init(newBlank);
-			newWindow.Closed += newWindow_Closed;
-			newWindow.Show();
+			if (CurrentFolder != null) {
+				TBPInfo newBlank = new TBPInfo();
+				newBlank.Number = "-";
+				newBlank.Name = "";
+				newBlank.FolderID = CurrentFolder.ID;
+				TBPWindow newWindow = new TBPWindow();
+				newBlank.EditingTBP = false;
+				newWindow.Init(newBlank);
+				newWindow.Closed += newWindow_Closed;
+				newWindow.Show();
+			}
 		}
 
 		void newWindow_Closed(object sender, EventArgs e) {
@@ -108,13 +113,17 @@ namespace MainSL {
 		}
 
 		private void btnUseNextTBP_Click(object sender, RoutedEventArgs e) {
-			GlobalContext.Single.IsBusy = true;
-			GlobalContext.Single.Client.InitTBPAsync(CurrentTBP);
+			if (CurrentTBP != null) {
+				GlobalContext.Single.IsBusy = true;
+				GlobalContext.Single.Client.InitTBPAsync(CurrentTBP);
+			}
 		}
 
 		private void btnUseOBP_Click(object sender, RoutedEventArgs e) {
-			GlobalContext.Single.IsBusy = true;
-			GlobalContext.Single.Client.InitOBPAsync(CurrentTBP);
+			if (CurrentTBP != null) {
+				GlobalContext.Single.IsBusy = true;
+				GlobalContext.Single.Client.InitOBPAsync(CurrentTBP);
+			}
 		}
 
 		void Client_InitOBPCompleted(object sender, InitOBPCompletedEventArgs e) {
@@ -155,38 +164,47 @@ namespace MainSL {
 			}
 		}
 
-		private void btnNewOBP_Click(object sender, RoutedEventArgs e) {
+		private void btnNewOBP_Click(object sender, RoutedEventArgs e) {			
 			TBPInfo tbp = new TBPInfo();
 			tbp.Number = "-";
 			tbp.Name = "ОБП";
+			tbp.FolderID = -1;
 			GlobalContext.Single.IsBusy = true;
 			GlobalContext.Single.Client.InitOBPAsync(tbp);
 		}
 
 		private void btnShowPDF_Click(object sender, RoutedEventArgs e) {
-			FloatWindow.OpenWindow("Home/getFile?id=" + CurrentTBP.IDPDFData);
+			if (CurrentTBP != null) {
+				FloatWindow.OpenWindow("Home/getFile?id=" + CurrentTBP.IDPDFData);
+			}
 		}
 
 		private void btnShowWord_Click(object sender, RoutedEventArgs e) {
-			FloatWindow.OpenWindow("Home/getFile?id=" + CurrentTBP.IDWordData);
+			if (CurrentTBP != null) {
+				FloatWindow.OpenWindow("Home/getFile?id=" + CurrentTBP.IDWordData);
+			}
 		}
 
 		private void btnEditTBP_Click(object sender, RoutedEventArgs e) {
-			TBPWindow newWindow = new TBPWindow();
-			CurrentTBP.EditingTBP = true;
-			newWindow.Init(CurrentTBP);
-			newWindow.Closed += newWindow_Closed;
-			newWindow.Show();
+			if (CurrentTBP != null) {
+				TBPWindow newWindow = new TBPWindow();
+				CurrentTBP.EditingTBP = true;
+				newWindow.Init(CurrentTBP);
+				newWindow.Closed += newWindow_Closed;
+				newWindow.Show();
+			}
 		}
 
 		private void btnCommentTBP_Click(object sender, RoutedEventArgs e) {
-			CommentWindow commentWin = new CommentWindow();
-			CurrentTBP.EditingTBP = true;
-			TBPComment newCom=new TBPComment();
-			newCom.TBPNumber = CurrentTBP.Number;
-			commentWin.Init(newCom);
-			commentWin.Closed += commentWin_Closed;
-			commentWin.Show();
+			if (CurrentTBP != null) {
+				CommentWindow commentWin = new CommentWindow();
+				CurrentTBP.EditingTBP = true;
+				TBPComment newCom = new TBPComment();
+				newCom.TBPNumber = CurrentTBP.Number;
+				commentWin.Init(newCom);
+				commentWin.Closed += commentWin_Closed;
+				commentWin.Show();
+			}
 		}
 
 		void commentWin_Closed(object sender, EventArgs e) {
@@ -204,7 +222,9 @@ namespace MainSL {
 		}
 
 		private void btnShowWordOBP_Click(object sender, RoutedEventArgs e) {
-			FloatWindow.OpenWindow("Home/getOBPWord?id=" + CurrentTBP.Number);
+			if (CurrentTBP != null) {
+				FloatWindow.OpenWindow("Home/getOBPWord?id=" + CurrentTBP.Number);
+			}
 		}
 
 		private void grdTBPBlanks_SelectionChanged(object sender, SelectionChangedEventArgs e) {
