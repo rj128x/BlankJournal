dbce47278791c1700aa95d8cd3ec481432d1a94b
rj128x <rj128x@gmail.com>
2016-01-12 15:51:31 +0500

    123

--- BlankJournal/Models/PDFClass.cs
+++ BlankJournal/Models/PDFClass.cs
@@ -16,15 +16,19 @@ namespace BlankJournal.Models {
 				PdfStamper pdfStamper = new PdfStamper(reader, new FileStream(newFileName, FileMode.Create, FileAccess.Write, FileShare.None));
 				Font f = new Font(bf, 12);
 				Phrase p = new Phrase(num, f);
-				PdfContentByte over = pdfStamper.GetOverContent(1);
+				float w = bf.GetWidthPoint(num, 12);
+				float h = bf.GetAscentPoint(num, 12);
+				//for (int i = 1; i <= reader.NumberOfPages; i++) {
+					PdfContentByte over = pdfStamper.GetOverContent(1);
 
-				Rectangle rect = reader.GetPageSizeWithRotation(1);
-				float x = (float)(rect.Width-rect.Width * 0.25);
-				float y = (float)(rect.Height - rect.Height *0.02);
+					Rectangle rect = reader.GetPageSizeWithRotation(1);
+					float x = (float)(rect.Width/2 - w/2);
+					float y = (float)(rect.Height-h*2);
 
-				ColumnText.ShowTextAligned(over, Element.ALIGN_CENTER, p, x, y, 0);
+					ColumnText.ShowTextAligned(over, Element.ALIGN_LEFT, p, x, y, 0);
 
-				over.SaveState();
+					over.SaveState();
+				//}
 				pdfStamper.FormFlattening = true;
 				pdfStamper.Close();
 			} catch (Exception e) {
