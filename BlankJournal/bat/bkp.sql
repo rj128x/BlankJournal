99c89fdd98d4ccbaa3ae3707dfc8e86fe92c8640
rj128x <rj128x@gmail.com>
2016-01-25 10:25:11 +0500

    156

--- BlankJournal/bat/bkp.sql
+++ BlankJournal/bat/bkp.sql
@@ -2,7 +2,7 @@ DECLARE @filedate VARCHAR(20)
 SET @filedate=REPLACE(REPLACE(CONVERT(VARCHAR(20),GETDATE(),20),':','.'),' ','_')
 
 DECLARE @file_path VARCHAR(256)
-SET @file_path='c:\int\BlanksBCK\Blanks_'+@filedate+'.bak'
+SET @file_path='c:\int\BlanksBKP\Blanks_'+@filedate+'.bak'
 
 BACKUP DATABASE [Blanks] TO  DISK = @file_path WITH NOFORMAT, INIT,  NAME = N'Blanks', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
 GO
