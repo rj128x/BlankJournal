4e71455ce515e98d695aae2919947c2b89cf292c
rj128x <rj128x@gmail.com>
2016-07-27 12:48:10 +0500

    123

--- BlankJournal/Models/FileSync.cs
+++ BlankJournal/Models/FileSync.cs
@@ -19,7 +19,14 @@ namespace BlankJournal.Models {
 				string path = string.Format(@"{0}\{1}", Settings.Single.archiveFolder, folder.Id + " " + folder.Name);
 
 				bool ok = true;
-				string shortname = tbl.Name.Length < 100 ? tbl.Name : tbl.Name.Substring(0, 100);
+				string shortname = "";
+				if (tbl.Name.Contains('(') && tbl.Name.Length>90) {
+					int ind = tbl.Name.IndexOf("(");
+					shortname = tbl.Name.Substring(0, ind);
+				} else {					
+					shortname = tbl.Name.Length>90?tbl.Name.Substring(0,90):tbl.Name;	
+				}			
+
 				if (!string.IsNullOrEmpty(tbl.DataPDF)) {
 					string name = tbl.Number + " " + shortname + ".pdf";
 					ok = ok && SyncFile(tbl.DataPDF, path, name, tbl.Number + " ");
@@ -41,7 +48,12 @@ namespace BlankJournal.Models {
 							string number = "ОБП " + tbl.Number + " ";
 							foreach (FileInfo fi in files) {
 								if (fi.Name.IndexOf(number) == 0) {
-									fi.Delete();
+									try {
+										Logger.info("Удаление файла " + fi.Name);
+										fi.Delete();
+									} catch (Exception e) {
+										Logger.info("Ошибка при удалении файла " + e.ToString());
+									}
 								}
 							}
 
@@ -62,7 +74,7 @@ namespace BlankJournal.Models {
 		}
 
 		public static bool SyncFile(string id, string path, string fileName, string number) {
-			Logger.info("Запись файла " + id);
+			Logger.info("Запись файла " + id +" в файл "+fileName);
 			try {
 				using (Impersonator imp = new Impersonator(Settings.Single.syncUser, Settings.Single.syncDomain, Settings.Single.syncPassword)) {
 
@@ -73,12 +85,17 @@ namespace BlankJournal.Models {
 					}
 					path = path + (tbl.isPDF ? @"\PDF" : "");
 					if (!Directory.Exists(path))
-						Directory.CreateDirectory(path);
+						Directory.CreateDirectory(path);					
 					DirectoryInfo di = new DirectoryInfo(path);
 					FileInfo[] files = di.GetFiles();
 					foreach (FileInfo fi in files) {
 						if (fi.Name.IndexOf(number) == 0) {
-							fi.Delete();
+							try {
+								Logger.info("Удаление файла " + fi.Name);
+								fi.Delete();
+							}catch (Exception e) {
+								Logger.info("Ошибка при удалении файла " + e.ToString());
+							}
 						}
 					}
 					System.IO.File.WriteAllBytes(path + "\\" + fileName, tbl.Data);
